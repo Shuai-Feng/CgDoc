c16ca9ed7dff018035f8d4e847ade9b6a22aa5b8
shuaifeng <shuaifeng121314@126.com>
2021-06-16 20:21:28 +0800

    pdf导出完成

--- src/pages/ICU/index.tsx
+++ src/pages/ICU/index.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useRequest } from 'ahooks';
+// 给组件引进来
+import menuConfig from './menuConfig';
+
+import { Chart, Interval, Annotation } from 'bizcharts';
+
 interface IICUpageProps {}
 
 const ICUpage: React.FunctionComponent<IICUpageProps> = props => {
@@ -11,19 +16,54 @@ const ICUpage: React.FunctionComponent<IICUpageProps> = props => {
       });
     });
   }
+
+  const data = [
+    { name: 'London', 月份: 'Jan.', 月均降雨量: 18.9 },
+    { name: 'London', 月份: 'Feb.', 月均降雨量: 28.8 },
+    { name: 'London', 月份: 'Mar.', 月均降雨量: 39.3 },
+    { name: 'London', 月份: 'Apr.', 月均降雨量: 81.4 },
+    { name: 'London', 月份: 'May', 月均降雨量: 47 },
+    { name: 'London', 月份: 'Jun.', 月均降雨量: 20.3 },
+    { name: 'London', 月份: 'Jul.', 月均降雨量: 24 },
+    { name: 'London', 月份: 'Aug.', 月均降雨量: 35.6 },
+    { name: 'Berlin', 月份: 'Jan.', 月均降雨量: 12.4 },
+    { name: 'Berlin', 月份: 'Feb.', 月均降雨量: 23.2 },
+    { name: 'Berlin', 月份: 'Mar.', 月均降雨量: 34.5 },
+    { name: 'Berlin', 月份: 'Apr.', 月均降雨量: 99.7 },
+    { name: 'Berlin', 月份: 'May', 月均降雨量: 52.6 },
+    { name: 'Berlin', 月份: 'Jun.', 月均降雨量: 35.5 },
+    { name: 'Berlin', 月份: 'Jul.', 月均降雨量: 37.4 },
+    { name: 'Berlin', 月份: 'Aug.', 月均降雨量: 42.4 },
+  ];
+
   useEffect(() => {
     axios.get('https://randomuser.me/api').then(res => {
       console.log(res.data.results[0].phone);
     });
   }, []);
-  const { data, loading, run, cancel } = useRequest(getUsername, {
-    pollingInterval: 3000,
+  const { data: myDate, loading, run, cancel } = useRequest(getUsername, {
+    pollingInterval: 1000,
     pollingWhenHidden: false,
   });
 
+  useEffect(() => {}, []);
+
   return (
     <div className="ICU_page">
-      <p>Username: {loading ? 'loading' : data}</p>asdfasd
+      {myDate}
+      <Chart height={400} padding="auto" data={data} autoFit>
+        <Interval
+          adjust={[
+            {
+              type: 'dodge',
+              marginRatio: 0,
+            },
+          ]}
+          color="name"
+          position="月份*月均降雨量"
+        />
+        <Annotation.Line start={['min', 'median']} end={['max', 'median']} />
+      </Chart>
     </div>
   );
 };
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,191 +1,251 @@
 import * as React from 'react';
-import { Card,Button,Table,Drawer,Badge,Tag,message,Modal,Descriptions} from 'antd';
+import {
+  Card,
+  Button,
+  Table,
+  Drawer,
+  Badge,
+  Tag,
+  message,
+  Modal,
+  Descriptions,
+  DatePicker,
+} from 'antd';
 import EditForm from './component/EditForm';
-import Axios from '@/utils/axios';  
+import Axios from '@/utils/axios';
 const DescrItem = Descriptions.Item;
+import zh_CN from 'antd/lib/locale/zh_CN';
+import locale from 'antd/es/date-picker/locale/zh_CN';
+
 //用于展示表格数据的接口
 interface medicItem {
-  medic_id:string
-  medic_status:number
-  medic_name:string
-  medic_dept:string
-  medic_num:number
-  create_time:Date
-  user_name:string
+  medic_id: string;
+  medic_status: number;
+  medic_name: string;
+  medic_dept: string;
+  medic_num: number;
+  create_time: Date;
+  user_name: string;
 }
 
 export interface IOrderState {
-    //表格数据
-    dataSource:any,
-    //判断是否打开侧边抽屉
-    isDrawerVisbel:boolean,
-    //用于存放未完成订单数量
-    orderNum:number
-    //操作类型
-    operaType:string
-    //表格行得id
-    selectedRowKeys:Array<number>
-    //选择改行得内容
-    selectedRowItem:Array<medicItem>
+  //表格数据
+  dataSource: any;
+  //判断是否打开侧边抽屉
+  isDrawerVisbel: boolean;
+  //用于存放未完成订单数量
+  orderNum: number;
+  //操作类型
+  operaType: string;
+  //表格行得id
+  selectedRowKeys: Array<number>;
+  //选择改行得内容
+  selectedRowItem: Array<medicItem>;
 }
 
 export default class Order extends React.Component<{}, IOrderState> {
   state = {
-    dataSource:undefined,
-    isDrawerVisbel:false,
-    selectedRowKeys:[],
-    selectedRowItem:[],
-    orderNum:0,
-    operaType:''
-  }
+    dataSource: undefined,
+    isDrawerVisbel: false,
+    selectedRowKeys: [],
+    selectedRowItem: [],
+    orderNum: 0,
+    operaType: '',
+  };
   //负责请求数据
-  requestList = async ()=>{
+  requestList = async () => {
     // 使用 async 和 await 进行订单设置
-    let dataSource:any = await Axios.ajax({url:"/order/list",data:{}})
-    let orderNum:number = 0;
-     dataSource = dataSource.result.item_list.map((item:any,index:number)=>{
-        item.key = index
-        if(!item.medic_status){
-          orderNum++
-        }
-        return item
-    })
+    let dataSource: any = await Axios.ajax({ url: '/order/list', data: {} });
+    let orderNum: number = 0;
+    dataSource = dataSource.result.item_list.map((item: any, index: number) => {
+      item.key = index;
+      if (!item.medic_status) {
+        orderNum++;
+      }
+      return item;
+    });
     this.setState({
       dataSource,
-      orderNum
-    })
-  }
+      orderNum,
+    });
+  };
 
   //负责删除数据
-  handleOrderDelete = (itemKey:string)=>{
-    Axios.ajax({url:'/order/delete',data:{
-      params:{
-        itemKey
-      }
-    }}).then(res=>{
+  handleOrderDelete = (itemKey: string) => {
+    Axios.ajax({
+      url: '/order/delete',
+      data: {
+        params: {
+          itemKey,
+        },
+      },
+    }).then(res => {
       message.success('订单删除成功');
       //当然，删完得刷新一下
       this.requestList();
-    })
-  }
-  handleOperation = (type:string)=>{
-    if(!this.state.selectedRowKeys.length){
-      Modal.info({title:"请选择一条记录"})
-      return 
+    });
+  };
+  handleOperation = (type: string) => {
+    if (!this.state.selectedRowKeys.length) {
+      Modal.info({ title: '请选择一条记录' });
+      return;
     }
-    let rowData:medicItem = this.state.selectedRowItem[0]||{};
+    let rowData: medicItem = this.state.selectedRowItem[0] || {};
 
-    if(['edit','view'].includes(type)){
+    if (['edit', 'view'].includes(type)) {
       this.setState({
-        isDrawerVisbel:true,
-        operaType:type
-      })
-    }else{
+        isDrawerVisbel: true,
+        operaType: type,
+      });
+    } else {
       Modal.confirm({
-        title:`您确定删除订单 ${rowData.medic_id} 吗？`,
-        onOk:()=>{
-          this.handleOrderDelete(rowData.medic_id)
+        title: `您确定删除订单 ${rowData.medic_id} 吗？`,
+        onOk: () => {
+          this.handleOrderDelete(rowData.medic_id);
         },
         onCancel() {
           console.log('Cancel');
         },
-      })
+      });
     }
-  }
-  handleOperaType = (type:string):any=>{
+  };
+  handleOperaType = (type: string): any => {
     return {
-      'view':'订单详情',
-      'edit':'订单修改',
-      'delete':'订单删除'
-    }[type]
-  }
+      view: '订单详情',
+      edit: '订单修改',
+      delete: '订单删除',
+    }[type];
+  };
   //这部分是表格选择得函数
-  handleSelect = (selectedRowKeys:any,selectedRowItem:any)=>{
+  handleSelect = (selectedRowKeys: any, selectedRowItem: any) => {
     this.setState({
       selectedRowKeys,
-      selectedRowItem
-    })
-  }
-  componentDidMount(){
-    this.requestList()
+      selectedRowItem,
+    });
+  };
+  componentDidMount() {
+    this.requestList();
   }
   public render() {
-    let { orderNum,selectedRowKeys,selectedRowItem } = this.state;
-    let rowData:medicItem = selectedRowItem[0]||{};
-    const rowSelection:any = {
-      type:'radio',
+    let { orderNum, selectedRowKeys, selectedRowItem } = this.state;
+    let rowData: medicItem = selectedRowItem[0] || {};
+    const rowSelection: any = {
+      type: 'radio',
       selectedRowKeys,
-      onChange:this.handleSelect
-    }
+      onChange: this.handleSelect,
+    };
     //为表格的列进行一下设置
     const columns = [
       {
-        title:"药品id",
-        key:'medic_id',
-        dataIndex:'medic_id'
+        title: '药品id',
+        key: 'medic_id',
+        dataIndex: 'medic_id',
       },
       {
-        title:"药品名称",
-        key:'medic_name',
-        dataIndex:'medic_name'
+        title: '药品名称',
+        key: 'medic_name',
+        dataIndex: 'medic_name',
       },
       {
-        title:"订单状态",
-        key:"medic_status",
-        dataIndex:"medic_status",
-        render:(value:number)=>{
-          if(value){
-            return <span><Badge color='green' />{'已完成'}</span>
+        title: '订单状态',
+        key: 'medic_status',
+        dataIndex: 'medic_status',
+        render: (value: number) => {
+          if (value) {
+            return (
+              <span>
+                <Badge color="green" />
+                {'已完成'}
+              </span>
+            );
           }
-          return <span><Badge color='red' />{'未完成'}</span>
-        }
-      },
-      {
-        title:"药品数量",
-        key:'medic_num',
-        dataIndex:'medic_num',
-        sorter:(rowA:any, rowB:any)=>{
-          return rowA.medic_num - rowB.medic_num
+          return (
+            <span>
+              <Badge color="red" />
+              {'未完成'}
+            </span>
+          );
         },
       },
       {
-        title:"订单创建时间",
-        key:'create_time',
-        dataIndex:'create_time',
-        sorter:(rowA:any, rowB:any)=>{
-          return (+new Date(rowA.create_time)) - (+new Date(rowB.create_time))
+        title: '药品数量',
+        key: 'medic_num',
+        dataIndex: 'medic_num',
+        sorter: (rowA: any, rowB: any) => {
+          return rowA.medic_num - rowB.medic_num;
         },
       },
       {
-        title:"创建用户",
-        key:'user_name',
-        dataIndex:'user_name'
+        title: '订单创建时间',
+        key: 'create_time',
+        dataIndex: 'create_time',
+        sorter: (rowA: any, rowB: any) => {
+          return +new Date(rowA.create_time) - +new Date(rowB.create_time);
+        },
       },
       {
-        title:"归属科室",
-        key:'medic_dept',
-        dataIndex:'medic_dept'
-      }
-    ]
- 
+        title: '创建用户',
+        key: 'user_name',
+        dataIndex: 'user_name',
+      },
+      {
+        title: '归属科室',
+        key: 'medic_dept',
+        dataIndex: 'medic_dept',
+      },
+    ];
+
     return (
       <div>
-        <Card style={{marginBottom:'10px'}}>
-          <Button style={{marginRight:10}} type='primary' onClick={()=>{this.handleOperation('view')}}>订单详情</Button>
-          <Button style={{marginRight:10}} type='primary' onClick={()=>{this.handleOperation('edit')}}>订单修改</Button>
-          <Button style={{marginRight:10}} type='primary' onClick={()=>{this.handleOperation('delete')}}>删除订单</Button>
-          <Button style={{float:"right"}} type='text'>
-              {
-                orderNum?<span>{orderNum}个订单未完成</span>:<Tag color="green">订单全部完成</Tag>
-              }
+        <Card style={{ marginBottom: '10px' }}>
+          <Button
+            style={{ marginRight: 10 }}
+            type="primary"
+            onClick={() => {
+              this.handleOperation('view');
+            }}
+          >
+            订单详情
+          </Button>
+          <Button
+            style={{ marginRight: 10 }}
+            type="primary"
+            onClick={() => {
+              this.handleOperation('edit');
+            }}
+          >
+            订单修改
+          </Button>
+          <Button
+            style={{ marginRight: 10 }}
+            type="primary"
+            onClick={() => {
+              this.handleOperation('delete');
+            }}
+          >
+            删除订单
+          </Button>
+          <Button style={{ float: 'right' }} type="text">
+            {orderNum ? (
+              <span>{orderNum}个订单未完成</span>
+            ) : (
+              <Tag color="green">订单全部完成</Tag>
+            )}
           </Button>
         </Card>
-        <div style={{background:"#fff",padding:"10px"}}>
-          <Table 
+        <div style={{ background: '#fff', padding: '10px' }}>
+          <DatePicker />
+          <Table
             columns={columns}
-            dataSource={this.state.dataSource}  
+            dataSource={this.state.dataSource}
             rowSelection={rowSelection}
-            scroll={document.documentElement.clientWidth<800?{x:'100%'}:{}}
+            scroll={
+              document.documentElement.clientWidth < 800 ? { x: '100%' } : {}
+            }
+            pagination={{
+              pageSize: 12,
+              total: 100,
+            }}
           />
         </div>
 
@@ -194,30 +254,36 @@ export default class Order extends React.Component<{}, IOrderState> {
           width={600}
           title={this.handleOperaType(this.state.operaType)}
           visible={this.state.isDrawerVisbel}
-          onClose={()=>{
+          onClose={() => {
             this.setState({
-              isDrawerVisbel:false
-            })
+              isDrawerVisbel: false,
+            });
           }}
         >
-          {
-            //如果我们是订单详情，我们就给他渲染一个Descritions 
-            this.state.operaType == 'view' ? 
-           <Descriptions layout='vertical' bordered title={rowData.medic_name}>
-                  <DescrItem label="编号">{rowData.medic_id}</DescrItem>  
-                  <DescrItem label="是否完成">{rowData.medic_status?"已完成":"未完成"}</DescrItem>
-                  <DescrItem label="药品数量">{rowData.medic_num}</DescrItem>
-                  <DescrItem label="配属科室">{rowData.medic_dept}</DescrItem>
-                  <DescrItem label="订单创建时间">{rowData.create_time}</DescrItem>
-                  <DescrItem label="创建人">{rowData.user_name}</DescrItem>
-             {/* 如果不是 那我们就加载咱自己加载的组件 */}
-            </Descriptions>:<EditForm  medicData={rowData} handleClose = {()=>{
-              this.setState({
-                isDrawerVisbel:false
-              })
-              this.requestList()
-            }} />
-          }
+          {//如果我们是订单详情，我们就给他渲染一个Descritions
+          this.state.operaType == 'view' ? (
+            <Descriptions layout="vertical" bordered title={rowData.medic_name}>
+              <DescrItem label="编号">{rowData.medic_id}</DescrItem>
+              <DescrItem label="是否完成">
+                {rowData.medic_status ? '已完成' : '未完成'}
+              </DescrItem>
+              <DescrItem label="药品数量">{rowData.medic_num}</DescrItem>
+              <DescrItem label="配属科室">{rowData.medic_dept}</DescrItem>
+              <DescrItem label="订单创建时间">{rowData.create_time}</DescrItem>
+              <DescrItem label="创建人">{rowData.user_name}</DescrItem>
+              {/* 如果不是 那我们就加载咱自己加载的组件 */}
+            </Descriptions>
+          ) : (
+            <EditForm
+              medicData={rowData}
+              handleClose={() => {
+                this.setState({
+                  isDrawerVisbel: false,
+                });
+                this.requestList();
+              }}
+            />
+          )}
         </Drawer>
       </div>
     );
