95b14a6f46a8b9428c25722e32e8141dc6a7b918
shuaifeng <shuaifeng121314@126.com>
2021-07-01 22:30:22 +0800

    科室数值求和

--- src/pages/ICU/index.tsx
+++ src/pages/ICU/index.tsx
@@ -1,154 +1,69 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useRequest } from 'ahooks';
-// 给组件引进来
-import menuConfig from './menuConfig';
-import TotalTree from './components/TotalTree';
-import { Chart, Interval, Annotation } from 'bizcharts';
+import * as React from 'react';
+import { Table } from 'antd';
 
 interface IICUpageProps {}
 
 const ICUpage: React.FunctionComponent<IICUpageProps> = props => {
-  function getUsername(): Promise<string> {
-    return new Promise(resolve => {
-      axios.get('https://randomuser.me/api').then(res => {
-        resolve(res.data.results[0].phone);
-      });
-    });
-  }
-
   const data = [
-    { name: 'London', 月份: 'Jan.', 月均降雨量: 18.9 },
-    { name: 'London', 月份: 'Feb.', 月均降雨量: 28.8 },
-    { name: 'London', 月份: 'Mar.', 月均降雨量: 39.3 },
-    { name: 'London', 月份: 'Apr.', 月均降雨量: 81.4 },
-    { name: 'London', 月份: 'May', 月均降雨量: 47 },
-    { name: 'London', 月份: 'Jun.', 月均降雨量: 20.3 },
-    { name: 'London', 月份: 'Jul.', 月均降雨量: 24 },
-    { name: 'London', 月份: 'Aug.', 月均降雨量: 35.6 },
-    { name: 'Berlin', 月份: 'Jan.', 月均降雨量: 12.4 },
-    { name: 'Berlin', 月份: 'Feb.', 月均降雨量: 23.2 },
-    { name: 'Berlin', 月份: 'Mar.', 月均降雨量: 34.5 },
-    { name: 'Berlin', 月份: 'Apr.', 月均降雨量: 99.7 },
-    { name: 'Berlin', 月份: 'May', 月均降雨量: 52.6 },
-    { name: 'Berlin', 月份: 'Jun.', 月均降雨量: 35.5 },
-    { name: 'Berlin', 月份: 'Jul.', 月均降雨量: 37.4 },
-    { name: 'Berlin', 月份: 'Aug.', 月均降雨量: 42.4 },
+    { type: '甲(妇产科)', sum: 10, avg: 3 },
+    { type: '已(妇产科)', sum: 21, avg: 2 },
+    { type: '丙(妇产科)', sum: 23, avg: 1 },
+    { type: '甲(男科)', sum: 10, avg: 6 },
+    { type: '已(男科)', sum: 21, avg: 6 },
+    { type: '丙(男科)', sum: 23, avg: 6 },
   ];
 
-  let treeArray = [
-    {
-      id: 1,
-      nodeName: 'a',
-      children: [
-        {
-          id: 11,
-          nodeName: 'ab',
-          children: [
-            {
-              id: 111,
-              nodeName: 'abc',
-            },
-            {
-              id: 112,
-              nodeName: 'abd',
-            },
-          ],
-        },
-        {
-          id: 12,
-          nodeName: 'ac',
-          children: [
-            {
-              id: 121,
-              nodeName: 'aca',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 2,
-      nodeName: 'b',
-    },
-    {
-      id: 3,
-      nodeName: 'c',
-      children: [
-        {
-          id: 31,
-          nodeName: 'cb',
-          children: [
-            {
-              id: 311,
-              nodeName: 'cbc',
-            },
-            {
-              id: 312,
-              nodeName: 'cbd',
-            },
-          ],
-        },
-        {
-          id: 32,
-          nodeName: 'cc',
-          children: [
-            {
-              id: 321,
-              nodeName: 'cca',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 4,
-      nodeName: 'd',
-    },
-  ];
+  const target = ['妇产科', '男科'];
 
-  const dfsPath = (tree: any[], key: string) => {
-    let nodePathArray = [];
+  let chartData = {};
 
-    // (tree为目标树，targetId为目标节点id)
-    function getNodeRoute(tree, targetId) {
-      for (let index = 0; index < tree.length; index++) {
-        if (tree[index].children) {
-          let endRecursiveLoop = getNodeRoute(tree[index].children, targetId);
-          if (endRecursiveLoop) {
-            nodePathArray.push(tree[index].id);
-            return true;
-          }
-        }
-        if (tree[index].id === targetId) {
-          nodePathArray.push(tree[index].id);
-          return true;
-        }
-      }
-    }
-    getNodeRoute(tree, key);
+  // 每一个类别都需要遍历
+  target.forEach(_t => {
+    // 科室初始化   分配基础空房间
+    // chartData[_t] = [];
 
-    return nodePathArray;
-  };
-
-  useEffect(() => {
-    axios.get('https://randomuser.me/api').then(res => {
-      console.log(res.data.results[0].phone);
-    });
-  }, []);
-
-  const { data: myDate, loading, run, cancel } = useRequest(getUsername, {
-    pollingInterval: 1000,
-    pollingWhenHidden: false,
+    chartData[_t] = data.filter(item => item.type.includes(_t));
+    // 遍历
+    // data.forEach(item => {
+    //   if (item.type.includes(_t)) {
+    //     chartData[_t].push(item);
+    //     // 这一步可以尝试让已分配出队  减少遍历时间
+    //     // 从 data 中 把找到的扔出去
+    //   }
+    // });
   });
 
-  useEffect(() => {
-    console.log(dfsPath(treeArray, 321));
-  }, []);
+  console.log(chartData); // 就差一步了 你需要让他们求和
+
+  let result = Object.keys(chartData).map(key => {
+    // 拿出所有科室的医生
+    let currentData = chartData[key];
+    let sum = 0; // 求和
+    let avg = 0; // 求均值
+
+    currentData.forEach(item => {
+      sum += item.sum;
+      avg += item.avg;
+    });
+
+    //
+    if (currentData.length) {
+      avg = avg / currentData.length;
+    }
+    return {
+      type: key,
+      sum,
+      avg,
+    };
+  });
+
+  console.log(result);
+  debugger;
 
   return (
-    <div className="ICU_page">
-      <TotalTree />
+    <div>
+      icuPage
+      <Table />
     </div>
   );
 };
